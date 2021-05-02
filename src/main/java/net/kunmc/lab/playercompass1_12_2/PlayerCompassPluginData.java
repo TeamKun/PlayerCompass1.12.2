6093488d7da6f6cb00b7fb232a47f2cecc515470
Maru <maru043@docomo.ne.jp>
2021-05-03 01:09:47 +0900

    configのセーブタイミングを変更

--- src/main/java/net/kunmc/lab/playercompass1_12_2/PlayerCompassPluginData.java
+++ src/main/java/net/kunmc/lab/playercompass1_12_2/PlayerCompassPluginData.java
@@ -2,7 +2,6 @@ package net.kunmc.lab.playercompass1_12_2;
 
 import org.bukkit.configuration.MemorySection;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,13 +14,6 @@ public class PlayerCompassPluginData {
         plugin.saveDefaultConfig();
         this.config = plugin.getConfig();
         this.plugin = plugin;
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                plugin.saveConfig();
-            }
-        }.runTaskTimerAsynchronously(plugin, 0, getSavePeriod());
     }
 
     public long getUpdatePointPeriod() {
@@ -32,14 +24,9 @@ public class PlayerCompassPluginData {
         set("UpdatePointPeriod", period);
     }
 
-    public void set(String key, Object value) {
-        boolean needSave = config.get(key) == null;
+    private void set(String key, Object value) {
         config.set(key, value);
-        if (needSave) plugin.saveConfig();
-    }
-
-    private long getSavePeriod() {
-        return config.getLong("SavePeriod");
+        plugin.saveConfig();
     }
 
     public Map<String, String> getApplicants() {
