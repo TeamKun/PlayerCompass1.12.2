95ff9cb92a635209e4b8c83651881786822fa7ea
Maru <maru043@docomo.ne.jp>
2021-05-09 19:06:04 +0900

    対象プレイヤーがオフラインでも距離が更新される仕様に変更

--- src/main/java/net/kunmc/lab/playercompass1_12_2/command/CompassTaskManager.java
+++ src/main/java/net/kunmc/lab/playercompass1_12_2/command/CompassTaskManager.java
@@ -3,10 +3,7 @@ package net.kunmc.lab.playercompass1_12_2.command;
 import net.kunmc.lab.playercompass1_12_2.PlayerCompassPlugin;
 import net.kunmc.lab.playercompass1_12_2.PlayerCompassPluginData;
 import net.kunmc.lab.playercompass1_12_2.utils.Utils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -42,7 +39,7 @@ public class CompassTaskManager {
         for (Map.Entry<String, CompassUpdateTask> entry : tasks.entrySet()) {
             CompassUpdateTask oldTask = entry.getValue();
             oldTask.cancel();
-            CompassUpdateTask newTask = new CompassUpdateTask(oldTask.senderName, oldTask.targetName);
+            CompassUpdateTask newTask = new CompassUpdateTask(oldTask.senderName, oldTask.targetName, oldTask.lastTargetLoc);
             newTask.runTaskTimerAsynchronously(PlayerCompassPlugin.getInstance(), 0, updatePeriod);
             tasks.put(entry.getKey(), newTask);
         }
@@ -52,10 +49,23 @@ public class CompassTaskManager {
     private class CompassUpdateTask extends BukkitRunnable {
         String senderName;
         String targetName;
+        Location lastTargetLoc;
 
         CompassUpdateTask(String senderName, String targetName) {
             this.senderName = senderName;
             this.targetName = targetName;
+            Player target = Bukkit.getPlayer(targetName);
+            if (target != null) {
+                this.lastTargetLoc = target.getLocation();
+            } else {
+                World overWorld = Bukkit.getWorlds().stream().filter(x -> x.getEnvironment().equals(World.Environment.NORMAL)).findFirst().get();
+                this.lastTargetLoc = new Location(overWorld, 0, 0, 0);
+            }
+        }
+
+        CompassUpdateTask(String senderName, String targetName, Location loc) {
+            this(senderName, targetName);
+            this.lastTargetLoc = loc;
         }
 
         @Override
@@ -64,12 +74,9 @@ public class CompassTaskManager {
             if (sender == null) return;
 
             Player target = Bukkit.getPlayer(targetName);
-            if (target == null) return;
-
-            Location loc = target.getLocation();
-            sender.setCompassTarget(loc);
-
-            String displayName = generateCompassName(target.getName(), loc, sender.getLocation());
+            if (target != null) lastTargetLoc = target.getLocation();
+            sender.setCompassTarget(lastTargetLoc);
+            String displayName = generateCompassName(targetName, lastTargetLoc, sender.getLocation());
 
             PlayerInventory inventory = sender.getInventory();
             inventory.forEach(item -> {
